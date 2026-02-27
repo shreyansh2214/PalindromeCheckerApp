f9d281a003ff1effa6b8fded724e9d4c46471b33
ananyasamal27 <ananyasamal2704@gmail.com>
2026-02-27 10:49:31 +0530

    UC9 Completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,18 +1,14 @@
 import java.util.*;
 public class PalindromeCheckerApp {
-    static boolean isPalindrome(String str){
-        str = str.replaceAll("\\s+", "").toLowerCase();
-        int start = 0, end = str.length() - 1;
-        while(start < end){
-            if(str.charAt(start) != str.charAt(end)) return false;
-            start++;
-            end--;
-        }
-        return true;
+    static boolean isPalindrome(String str, int start, int end){
+        if(start >= end) return true;
+        if(str.charAt(start) != str.charAt(end)) return false;
+        return isPalindrome(str, start + 1, end - 1);
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        System.out.println(isPalindrome(str) ? "Palindrome" : "Not Palindrome");
+        boolean result = isPalindrome(str, 0, str.length() - 1);
+        System.out.println(result ? "Palindrome" : "Not Palindrome");
     }
 }
