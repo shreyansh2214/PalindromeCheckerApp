f0915c32ba9b8e13eb2c0a25fe24a6adfd2d621c
ananyasamal27 <ananyasamal2704@gmail.com>
2026-02-27 10:31:26 +0530

    UC8 Completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,19 +1,50 @@
 import java.util.*;
 public class PalindromeCheckerApp {
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+        }
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter input:");
         String str = sc.nextLine();
-        Deque<Character> deque = new ArrayDeque<>();
+        Node head = null, tail = null;
         for(char c : str.toCharArray()){
-            deque.addLast(c);
+            Node newNode = new Node(c);
+            if(head == null){
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
         }
+        if(head == null || head.next == null){
+            System.out.println("Palindrome");
+            return;
+        }
+        Node slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node prev = null, current = slow, next = null;
+        while(current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        Node first = head, second = prev;
         boolean isPalindrome = true;
-        while(deque.size() > 1){
-            if(!deque.removeFirst().equals(deque.removeLast())){
+        while(second != null){
+            if(first.data != second.data){
                 isPalindrome = false;
                 break;
             }
+            first = first.next;
+            second = second.next;
         }
         System.out.println(isPalindrome ? "Palindrome" : "Not Palindrome");
     }
