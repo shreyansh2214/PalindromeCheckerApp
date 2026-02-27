33502af3cce38d86a3395dfa8304a51f0f6b27e7
ananyasamal27 <ananyasamal2704@gmail.com>
2026-02-27 09:31:55 +0530

    Welcome to UC1

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,15 +1,9 @@
-import java.util.Scanner;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter input: ");
-        String s1= sc.nextLine();
-        String original= s1.toLowerCase();
-        String rev= new StringBuilder(original).reverse().toString();
-        if(original.equals(rev)){
-            System.out.println("YES");
-        }else{
-            System.out.println("NO");
-        }
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("system initialized successfully");
+
     }
 }
