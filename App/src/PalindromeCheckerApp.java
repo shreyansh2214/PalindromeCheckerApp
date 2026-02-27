e95e3e73c159a28c15e91024744240129491806d
ananyasamal27 <ananyasamal2704@gmail.com>
2026-02-27 09:20:16 +0530

    UC3-Palindrom Check Using String Reverse

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter input: ");
+        String s1= sc.nextLine();
+        String original= s1.toLowerCase();
+        String rev= new StringBuilder(original).reverse().toString();
+        if(original.equals(rev)){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
