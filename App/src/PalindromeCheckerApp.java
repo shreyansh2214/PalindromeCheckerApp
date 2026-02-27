50a5861d473ac6bb5b6366c22a1f33b8e343a2cb
ananyasamal27 <ananyasamal2704@gmail.com>
2026-02-27 09:51:45 +0530

    UC3 Completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,16 +1,15 @@
-
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String s1="madam";
-        String s2="madam";
-        if(s1==s2){
-            System.out.println(s1);
-            System.out.println("Palindrome");
-        }
-        else{
-            System.out.println("Not Palindrome");
-        }
-
-
+                Scanner sc = new Scanner(System.in);
+                System.out.print("enter input: ");
+                String s1= sc.nextLine();
+                String original= s1.toLowerCase();
+                String rev= new StringBuilder(original).reverse().toString();
+                if(original.equals(rev)){
+                    System.out.println("YES");
+                }else{
+                    System.out.println("NO");
+                }
     }
 }
